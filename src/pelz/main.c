f4c05240c65b0193936303c00d90db72b29c4d24
Nick <31989480+PircDef@users.noreply.github.com>
2021-05-04 16:58:23 -0400

    Introduced command line options in pelz (#6)

--- src/pelz/main.c
+++ src/pelz/main.c
@@ -2,21 +2,75 @@
  * Contains the main function used to launch the Pelz Key Service
  */
 
+#include <getopt.h>
 #include <stdlib.h>
 
 #include "key_table.h"
 #include "pelz_service.h"
 #include "pelz_log.h"
 
+static void usage(const char *prog)
+{
+  fprintf(stdout,
+    "\nusage: %s [options] \n\n"
+    "options are: \n\n"
+    " -h or --help          Help (displays this usage).\n"
+    " -m or --max_requests  Maximum number of sockets pelz can make available at any given time, default: 100\n"
+    " -v or --verbose       Enable detailed logging.\n", prog);
+}
+
+const struct option longopts[] = {
+  {"help", no_argument, 0, 'h'},
+  {"max_requests", required_argument, 0, 'm'},
+  {"verbose", no_argument, 0, 'v'},
+  {0, 0, 0, 0}
+};
+
 //Main function for the Pelz Service application
 int main(int argc, char **argv)
 {
-  const int max_requests = 100;
+  if (argc == 1)
+  {
+    usage(argv[0]);
+    return 0;
+  }
 
   set_app_name("pelz");
   set_app_version("0.0.0");
   set_applog_path("/var/log/pelz.log");
   set_applog_severity_threshold(LOG_WARNING);
+
+  int max_requests = 100;
+  int options;
+  int option_index;
+  long mr = 0;
+
+  while ((options = getopt_long(argc, argv, "m:hv", longopts, &option_index)) != -1)
+  {
+    switch (options)
+    {
+    case 'h':
+      usage(argv[0]);
+      return 0;
+    case 'm':
+      if (optarg && (mr = atol(optarg)) > 0)
+      {
+        max_requests = (int) mr;
+        break;
+      }
+      else
+      {
+        pelz_log(LOG_ERR, "max_request must be an integer. Received invalid option '%s'", optarg);
+        return 1;
+      }
+    case 'v':
+      set_applog_severity_threshold(LOG_DEBUG);
+      set_applog_output_mode(0);
+      break;
+    default:
+      return 1;
+    }
+  }
 
   //Initializing Key Table with max key entries set to key_max
   if (key_table_init())
@@ -25,7 +79,7 @@ int main(int argc, char **argv)
     return (1);
   }
 
-  pelz_service(max_requests);
+  pelz_service((const int) max_requests);
   key_table_destroy();
   return (0);
 }
