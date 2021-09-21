8d7b14c211fd7be1de4567651af84715deeb04b7
b-carr <53411033+b-carr@users.noreply.github.com>
2021-09-21 15:06:00 -0400

    Added comments to header files.

--- include/pelz_key_loaders.h
+++ include/pelz_key_loaders.h
@@ -5,8 +5,17 @@
 extern "C"
 {
 #endif
-
-  int pelz_load_key_from_file(char *filename, size_t * key_len, unsigned char **key);
+/**
+ * @brief Loads a key from a specified key file.
+ *
+ * @param[in]     filename  The filename in a null-terminated string
+ * @param[in,out] key_len   A pointer to a size_t to hold the key length
+ * @param[in,out] key       A pointer to a location to allocate memory for 
+ *                          the key.
+ *
+ * @returns 0 on success, 1 on error
+ */
+int pelz_load_key_from_file(char *filename, size_t * key_len, unsigned char **key);
 
 #ifdef __cplusplus
 }
--- include/pelz_uri_helpers.h
+++ include/pelz_uri_helpers.h
@@ -13,9 +13,25 @@ typedef enum
 extern "C"
 {
 #endif
+/**
+ * @brief Returns the URI_SCHEME enum for the specified URI.
+ *
+ * @param[in] uri  The (already parsed) URI
+ *
+ * @return The URI_SCHEME enum, for URI_SCHEME_UNKNOWN if the
+ *         URI does not have a recognized scheme.
+ */
+URI_SCHEME get_uri_scheme(UriUriA uri);
 
-  URI_SCHEME get_uri_scheme(UriUriA uri);
-  char *get_filename_from_key_id(char *null_terminated_key_id);
+/**
+ * @brief Returns the filename from a null-terminated key_id.
+ *
+ * @param[in] null_terminated_key_id The key id embedded in a
+ *                                   null-terminated string
+ *
+ * @return The filename, or NULL on error.
+ */
+char *get_filename_from_key_id(char *null_terminated_key_id);
 #ifdef __cplusplus
 }
 #endif
