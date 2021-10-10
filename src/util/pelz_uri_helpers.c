105624850601a4c0d91d37c46e3b410e4c92f095
b-carr <53411033+b-carr@users.noreply.github.com>
2021-10-10 07:14:50 -0400

    Improved error handling for pelz URI parsing.

--- src/util/pelz_uri_helpers.c
+++ src/util/pelz_uri_helpers.c
@@ -1,5 +1,6 @@
 #include <uriparser/Uri.h>
 #include <stddef.h>
+#include <limits.h>
 
 #include "charbuf.h"
 #include "pelz_uri_helpers.h"
@@ -48,19 +49,44 @@ int get_pelz_uri_parts(UriUriA uri, charbuf * common_name, int *port, charbuf * 
   // Extract the hostname
   field_length = uri.hostText.afterLast - uri.hostText.first;
   *common_name = new_charbuf((size_t) field_length);
+  if (common_name->chars == NULL)
+  {
+    pelz_log(LOG_ERR, "Failed to initialize charbuf.");
+    return 1;
+  }
   memcpy(common_name->chars, uri.hostText.first, field_length);
 
   // Extract the port
   field_length = uri.pathHead->text.afterLast - uri.pathHead->text.first;
   char *port_text = (char *) calloc((1 + field_length), sizeof(char));
 
+  if (port_text == NULL)
+  {
+    pelz_log(LOG_ERR, "Failed to initialize memory.");
+    free_charbuf(common_name);
+    return 1;
+  }
   memcpy(port_text, uri.pathHead->text.first, field_length);
-  *port = strtol(port_text, NULL, 10);
+  long int port_long = strtol(port_text, NULL, 10);
+
   free(port_text);
+  if (port_long < 0 || port_long > INT_MAX)
+  {
+    pelz_log(LOG_ERR, "Invalid port specified: %ld", port_long);
+    free_charbuf(common_name);
+    return 1;
+  }
+  *port = (int) port_long;
 
   // Extract the key UID
   field_length = uri.pathHead->next->text.afterLast - uri.pathHead->next->text.first;
   *key_id = new_charbuf((size_t) field_length);
+  if (key_id->chars == NULL)
+  {
+    pelz_log(LOG_ERR, "Failed to initialize charbuf.");
+    free_charbuf(common_name);
+    return 1;
+  }
   memcpy(key_id->chars, uri.pathHead->next->text.first, field_length);
 
   // Extract any additional data
@@ -68,6 +94,13 @@ int get_pelz_uri_parts(UriUriA uri, charbuf * common_name, int *port, charbuf * 
   {
     field_length = uri.pathTail->text.afterLast - uri.pathHead->next->next->text.first;
     *additional_data = new_charbuf((size_t) field_length);
+    if (additional_data->chars == NULL)
+    {
+      pelz_log(LOG_ERR, "Failed to initialize charbuf.");
+      free_charbuf(key_id);
+      free_charbuf(common_name);
+      return 1;
+    }
     memcpy(additional_data->chars, uri.pathHead->next->next->text.first, field_length);
   }
   return 0;
