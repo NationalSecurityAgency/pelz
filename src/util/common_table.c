24636fb08ad88064d1c010310c754aa83db60cbc
JWZeta <12718816+JWZeta@users.noreply.github.com>
2021-11-15 10:28:37 -0500

    Comment fix

--- src/util/common_table.c
+++ src/util/common_table.c
@@ -32,58 +32,49 @@ Table server_table = {
 //Destroy server table
 int table_destroy(TableType type)
 {
-  Table table;
+  Table *table;
 
   pelz_log(LOG_DEBUG, "Table Destroy Function Starting");
 
   switch (type)
   {
   case KEY:
-    table = key_table;
+    table = &key_table;
     break;
   case SERVER:
-    table = server_table;
+    table = &server_table;
     break;
   default:
     return (1);
   }
 
-  for (unsigned int i = 0; i < table.num_entries; i++)
+  for (unsigned int i = 0; i < table->num_entries; i++)
   {
-    if (table.entries[i].id.len != 0)
+    if (table->entries[i].id.len != 0)
     {
-      free_charbuf(&table.entries[i].id);
+      free_charbuf(&table->entries[i].id);
     }
     if (type == KEY)
     {
-      if (table.entries[i].value.cert.len != 0)
+      if (table->entries[i].value.cert.len != 0)
       {
-        secure_free_charbuf(&table.entries[i].value.key);
+        secure_free_charbuf(&table->entries[i].value.key);
       }
     }
     if (type == SERVER)
     {
-      if (table.entries[i].value.cert.len != 0)
+      if (table->entries[i].value.cert.len != 0)
       {
-        secure_free_charbuf(&table.entries[i].value.cert);
+        secure_free_charbuf(&table->entries[i].value.cert);
       }
     }
   }
 
   //Free the storage allocated for the hash table
-  free(table.entries);
-  table.entries = NULL;
-  table.num_entries = 0;
-  table.mem_size = 0;
-
-  if (type == KEY)
-  {
-    key_table = table;
-  }
-  if (type == SERVER)
-  {
-    server_table = table;
-  }
+  free(table->entries);
+  table->entries = NULL;
+  table->num_entries = 0;
+  table->mem_size = 0;
 
   pelz_log(LOG_DEBUG, "Table Destroy Function Complete");
   return (0);
@@ -91,45 +82,45 @@ int table_destroy(TableType type)
 
 int table_delete(TableType type, charbuf id)
 {
-  Table table;
+  Table *table;
   int index = 0;
 
   switch (type)
   {
   case KEY:
-    table = key_table;
+    table = &key_table;
     break;
   case SERVER:
-    table = server_table;
+    table = &server_table;
     break;
   default:
     return (1);
   }
 
-  for (unsigned int i = 0; i < table.num_entries; i++)
+  for (unsigned int i = 0; i < table->num_entries; i++)
   {
-    if (cmp_charbuf(id, table.entries[i].id) == 0)
+    if (cmp_charbuf(id, table->entries[i].id) == 0)
     {
       if (type == KEY)
       {
-        table.mem_size =
-          table.mem_size - ((table.entries[i].value.key.len * sizeof(char)) + (table.entries[i].id.len * sizeof(char)) +
+        table->mem_size =
+          table->mem_size - ((table->entries[i].value.key.len * sizeof(char)) + (table->entries[i].id.len * sizeof(char)) +
           (2 * sizeof(size_t)));
       }
       else if (type == SERVER)
       {
-        table.mem_size =
-          table.mem_size - ((table.entries[i].value.cert.len * sizeof(char)) + (table.entries[i].id.len * sizeof(char)) +
+        table->mem_size =
+          table->mem_size - ((table->entries[i].value.cert.len * sizeof(char)) + (table->entries[i].id.len * sizeof(char)) +
           (2 * sizeof(size_t)));
       }
-      free_charbuf(&table.entries[i].id);
+      free_charbuf(&table->entries[i].id);
       if (type == KEY)
       {
-        secure_free_charbuf(&table.entries[i].value.key);
+        secure_free_charbuf(&table->entries[i].value.key);
       }
       if (type == SERVER)
       {
-        secure_free_charbuf(&table.entries[i].value.cert);
+        secure_free_charbuf(&table->entries[i].value.cert);
       }
       index = i + 1;
       break;
@@ -140,63 +131,54 @@ int table_delete(TableType type, charbuf id)
     pelz_log(LOG_ERR, "ID not found.");
     return NO_MATCH;
   }
-  else if (table.mem_size == 0)
+  else if (table->mem_size == 0)
   {
-    free(table.entries);
-    table.entries = NULL;
-    table.num_entries = 0;
+    free(table->entries);
+    table->entries = NULL;
+    table->num_entries = 0;
   }
   else
   {
-    for (unsigned int i = index; i < table.num_entries; i++)
+    for (unsigned int i = index; i < table->num_entries; i++)
     {
-      table.entries[i - 1] = table.entries[i];
+      table->entries[i - 1] = table->entries[i];
     }
-    table.num_entries -= 1;
+    table->num_entries -= 1;
 
     Entry *temp;
 
-    if ((temp = (Entry *) realloc(table.entries, (table.num_entries) * sizeof(Entry))) == NULL)
+    if ((temp = (Entry *) realloc(table->entries, (table->num_entries) * sizeof(Entry))) == NULL)
     {
       pelz_log(LOG_ERR, "List Space Reallocation Error");
       return ERR_REALLOC;
     }
     else
     {
-      table.entries = temp;
+      table->entries = temp;
     }
-  }
-
-  if (type == KEY)
-  {
-    key_table = table;
-  }
-  else if (type == SERVER)
-  {
-    server_table = table;
   }
   return OK;
 }
 
 int table_lookup(TableType type, charbuf id, int *index)
 {
-  Table table;
+  Table *table;
 
   switch (type)
   {
   case KEY:
-    table = key_table;
+    table = &key_table;
     break;
   case SERVER:
-    table = server_table;
+    table = &server_table;
     break;
   default:
     return (1);
   }
 
-  for (unsigned int i = 0; i < table.num_entries; i++)
+  for (unsigned int i = 0; i < table->num_entries; i++)
   {
-    if (cmp_charbuf(id, table.entries[i].id) == 0)
+    if (cmp_charbuf(id, table->entries[i].id) == 0)
     {
       *index = i;
       return (0);
