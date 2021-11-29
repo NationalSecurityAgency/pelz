473154e58d7fb79e1e3c3c10f3f2cbef27bc856f
JWZeta <12718816+JWZeta@users.noreply.github.com>
2021-11-29 11:48:40 -0500

    Add function key_table_add_from_handle to key_table.c

--- src/util/key_table.c
+++ src/util/key_table.c
@@ -57,6 +57,60 @@ TableResponseStatus key_table_add_key(charbuf key_id, charbuf key)
   return OK;
 }
 
+TableResponseStatus key_table_add_from_handle(charbuf key_id, uint64_t handle)
+{
+  Entry tmp_entry;
+  size_t max_mem_size;
+  uint8_t *data;
+  size_t data_size = 0;
+
+  max_mem_size = 1000000;
+
+  if (key_table.mem_size >= max_mem_size)
+  {
+    pelz_log(LOG_ERR, "Key Table memory allocation greater then specified limit.");
+    return ERR_MEM;
+  }
+
+  tmp_entry.id = copy_chars_from_charbuf(key_id, 0);
+
+  data_size = retrieve_from_unseal_table(handle, &data);
+  if (data_size == 0)
+  {
+    pelz_log(LOG_ERR, "Failure to retrive data from unseal table.");
+    return RET_FAIL;
+  }
+
+  tmp_entry.value.key = new_charbuf(data_size);
+  if (data_size != tmp_entry.value.key.len)
+  {
+    pelz_log(LOG_ERR, "Charbuf creation error.");
+    return ERR_BUF;
+  }
+  memcpy(tmp_entry.value.key.chars, data, tmp_entry.value.key.len);
+
+  Entry *temp;
+
+  if ((temp = (Entry *) realloc(key_table.entries, (key_table.num_entries + 1) * sizeof(Entry))) == NULL)
+  {
+    pelz_log(LOG_ERR, "Key List Space Reallocation Error");
+    free_charbuf(&tmp_entry.id);
+    secure_free_charbuf(&tmp_entry.value.key);
+    return ERR_REALLOC;
+  }
+  else
+  {
+    key_table.entries = temp;
+  }
+
+  key_table.entries[key_table.num_entries] = tmp_entry;
+  key_table.num_entries++;
+  key_table.mem_size =
+    key_table.mem_size + ((tmp_entry.value.key.len * sizeof(char)) + (tmp_entry.id.len * sizeof(char)) + (2 * sizeof(size_t)));
+  pelz_log(LOG_INFO, "Key Added");
+  return OK;
+}
+
 TableResponseStatus key_table_add_from_server(charbuf key_id, charbuf server_id, charbuf server_key_id)
 {
   Entry tmp_entry;
