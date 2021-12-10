5afa1bd200d82441979c7d5771f208263750ea75
JWZeta <12718816+JWZeta@users.noreply.github.com>
2021-12-10 09:09:28 -0500

    Update to aes_keywrap_3394nopad

--- include/aes_keywrap_3394nopad.h
+++ include/aes_keywrap_3394nopad.h
@@ -1,60 +1,63 @@
 /** 
- * @file aes_keywrap_3394nopad.h
- * @brief Provides access to OpenSSL's AES Key Wrap (RFC 3394).
+ * @file  aes_keywrap_3394nopad.h
  *
+ * @brief Provides access to OpenSSL's AES Key Wrap (RFC 3394) for pelz.
  */
 #ifndef AES_KEYWRAP_3394NOPAD_H
-#define AES_KEYWRAP_3394NOPAD_H
+#define AES_KEYWRAP_3394NOPAD_H_
 
-#include <stdbool.h>
-#include <stdint.h>
 #include <stdlib.h>
-#include <stddef.h>
-
-#ifdef __cplusplus
-extern "C"
-{
-#endif
 
 /**
- * <pre>
+ * @brief This function uses OpenSSL to perform AES key wrap without padding
+ *        (RFC 3394).
  *
- * This function uses openssl to perform AES key wrap without padding (RFC 3394).
- * </pre>
- * @param[in] aes_key the hext bytes containing the key.
- * @param[in] key_len the length (in bytes) of the AES key (must be 16, 24, of 32).
- * @param[in] inData the plaintext data to be wrapped.
- * @param[in] inData_len the length of the plaintext data in bytes.
- * @param[out] outData the output ciphertext.
- * @param[out] outData_len the length of the output ciphertext in bytes.
- * @param[in] verbose if true, print extra debug messages.
+ * @param[in]  key         The hex bytes containing the key -
+ *                         pass in pointer to key value
+ *
+ * @param[in]  key_len     The length (in bytes) of the AES key
+ *                         (must be 16, 24, of 32)
+ *
+ * @param[in]  inData      The plaintext data to be wrapped -
+ *                         pass in pointer to input plaintext buffer
+ *
+ * @param[in]  inData_len  The length of the plaintext data in bytes
+ *
+ * @param[out] outData     The output ciphertext data -
+ *                         pass as pointer to address of buffer
+ *
+ * @param[out] outData_len The length of the output ciphertext in bytes -
+ *                         pass as pointer to length value
  *
  * @return 0 on success, 1 on error
- *
  */
-  int aes_keywrap_3394nopad_encrypt(unsigned char *key,
-    size_t key_len, unsigned char *inData, size_t inData_len, unsigned char **outData, size_t * outData_len);
+int aes_keywrap_3394nopad_encrypt(unsigned char *key,
+  size_t key_len, unsigned char *inData, size_t inData_len, unsigned char **outData, size_t * outData_len);
 
 /**
- * <pre>
+ * @brief This function uses OpenSSL to perform AES key unwrap without padding
+ *        (RFC 3394).
  *
- * This function uses openssl to perform AES key unwrap without padding (RFC 3394). 
- * </pre>
- * @param[in] aes_key the hex bytes containing the key.
- * @param[in] key_len the length (in bytes) of the AES key (must be 16, 24, or 32).
- * @param[in] inData the encrypted data to be unwrapped.
- * @param[in] inData_len the length of the encrypted data in bytes.
- * @param[out] outData the output plaintext
- * @param[out] outData_len the length in bytes of the output plaintext.
- * @param[in] verbose if true, print extra debug messages
- * 
+ * @param[in]  key         The hex bytes containing the key -
+ *                         pass in pointer to key value
+ *
+ * @param[in]  key_len     The length (in bytes) of the AES key
+ *                         (must be 16, 24, or 32)
+ *
+ * @param[in]  inData      The encrypted data to be unwrapped -
+ *                         pass in pointer to input buffer
+ *
+ * @param[in]  inData_len  The length of the encrypted data in bytes
+ *
+ * @param[out] outData     The output plaintext buffer -
+ *                         pass as pointer to address of buffer
+ *
+ * @param[out] outData_len The length in bytes of the output plaintext -
+ *                         pass as pointer to length value
+ *
  * @return 0 on success, 1 on error
- *
  */
-  int aes_keywrap_3394nopad_decrypt(unsigned char *aes_key,
-    size_t key_len, unsigned char *inData, size_t inData_len, unsigned char **outData, size_t * outData_len);
+int aes_keywrap_3394nopad_decrypt(unsigned char *key,
+  size_t key_len, unsigned char *inData, size_t inData_len, unsigned char **outData, size_t * outData_len);
 
-#ifdef __cplusplus
-}
-#endif
 #endif
