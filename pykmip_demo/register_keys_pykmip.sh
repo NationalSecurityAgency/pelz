f3ff11a09581ae9140f2dda26bc0514404352034
JWZeta <12718816+JWZeta@users.noreply.github.com>
2022-03-23 12:41:06 -0400

    Changed PyKMIP KEK from 128 to 256

--- pykmip_demo/register_keys_pykmip.sh
+++ pykmip_demo/register_keys_pykmip.sh
@@ -10,8 +10,10 @@ c = client.ProxyKmipClient()
 
 symmetric_key = objects.SymmetricKey(
      enums.CryptographicAlgorithm.AES,
-     128,
+     256,
      (
+         b'\x00\x01\x02\x03\x04\x05\x06\x07'
+         b'\x08\x09\x0A\x0B\x0C\x0D\x0E\x0F'
          b'\x00\x01\x02\x03\x04\x05\x06\x07'
          b'\x08\x09\x0A\x0B\x0C\x0D\x0E\x0F'
      )
