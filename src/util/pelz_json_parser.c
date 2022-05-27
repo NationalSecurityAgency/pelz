fc82d4496114e1fc1505926fa91c0f161c442e1f
b-carr <53411033+b-carr@users.noreply.github.com>
2022-05-27 07:22:52 -0400

    Added helper functions and started replacing JSON field extraction with calls to helper functions.

--- src/util/pelz_json_parser.c
+++ src/util/pelz_json_parser.c
@@ -12,6 +12,68 @@
 #include <charbuf.h>
 #include <pelz_log.h>
 
+/**
+ * <pre>
+ * Helper function to extract fields from JSON structs. The fields must have
+ * an associated length field whose name is <field_name>_len.
+ * <pre>
+ *
+ * @param[in] json       The JSON structure.
+ * @param[in] field_name The name of the desired field.
+ *
+ * @return A charbuf containing the data from the field, or a charbuf
+ *         of length 0 on error.
+ */
+static charbuf get_JSON_string_field(cJSON* json, const char* field_name)
+{
+  charbuf field;
+  if(!cJSON_HasObjectItem(json, field_name) || !cJSON_IsString(cJSON_GetObjectItem(json, field_name)))
+  {
+    pelz_log(LOG_ERR, "Missing JSON field %s.", field_name);
+    return new_charbuf(0);
+  } 
+  if(cJSON_GetObjectItemCaseSensitive(json, field_name)->valuestring != NULL)
+  {
+    field = new_charbuf(strlen(cJSON_GetObjectItemCaseSensitive(json, field_name)->valuestring));
+    if(field.len == 0 || field.chars == NULL)
+    {  
+      pelz_log(LOG_ERR, "Failed to allocate memory to extract JSON field %s.", field_name);
+      return new_charbuf(0);
+    }
+    memcpy(field.chars, cJSON_GetObjectItemCaseSensitive(json, field_name)->valuestring, field.len);
+  }
+  else
+  {
+    pelz_log(LOG_ERR, "No value in JSON field %s.", field_name);
+    return new_charbuf(0);
+  }
+  return field;
+}
+
+/**
+ * <pre>
+ * Helper function to extract fields from JSON structs. The fields must have
+ * an associated length field whose name is <field_name>_len.
+ * <pre>
+ *
+ * @param[in]  json       The JSON structure.
+ * @param[in]  field_name The name of the desired field.
+ * @param[out] value      Integer pointer to hold the extracted value.
+ *
+ * @return 0 on success, 1 error
+ */
+static int get_JSON_int_field(cJSON* json, const char* field_name, int* value)
+{
+  if(!cJSON_HasObjectItem(json, field_name) || !cJSON_IsNumber(cJSON_GetObjectItem(json, field_name)))
+  {
+    pelz_log(LOG_ERR, "Missing JSON field %s.", field_name);
+    return 1;
+  }
+  *value = cJSON_GetObjectItemCaseSensitive(json, field_name)->valueint;
+  return 0;
+}
+
+
 int request_decoder(charbuf request, RequestType * request_type, charbuf * key_id, charbuf * data, charbuf * request_sig, charbuf * requestor_cert)
 {
   cJSON *json;
@@ -21,19 +83,13 @@ int request_decoder(charbuf request, RequestType * request_type, charbuf * key_i
   memcpy(str, request.chars, request.len);
   json = cJSON_Parse(str);
   free(str);
-  if (!cJSON_HasObjectItem(json, "request_type"))
+  if(get_JSON_int_field(json, "request_type", (int*)request_type))
   {
     pelz_log(LOG_ERR, "Missing required JSON key: request_type.");
     cJSON_Delete(json);
     return (1);
   }
-  else if (!cJSON_IsNumber(cJSON_GetObjectItem(json, "request_type")))
-  {
-    pelz_log(LOG_ERR, "Incorrect data type of JSON value of JSON key: request_type. Data type should be integer.");
-    cJSON_Delete(json);
-    return (1);
-  }
-  *request_type = (RequestType) cJSON_GetObjectItemCaseSensitive(json, "request_type")->valueint;
+
   switch (*request_type)
   {    
   case REQ_ENC:
