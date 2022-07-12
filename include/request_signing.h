6ebbf6196ee23ac37f15f2db8e08a096e0fcd279
arp102 <92389169+arp102@users.noreply.github.com>
2022-07-12 13:42:39 -0400

    Fix inaccurate header comments.

--- include/request_signing.h
+++ include/request_signing.h
@@ -12,14 +12,14 @@
 
 /**
  * <pre>
- * Validation function. The validation process will determine if the supplied signature and certificate match
+ * Validate the signature and certificate for a Pelz request.
  * <pre>
  *
- * @param[in] json Parsed json string in cJSON format to be copied into request values
- *
- * @param[out] data.chars The data to be encrypted
- * @param[out] request_sig.chars The supplied signature
- * @param[out] requestor_cert.len The supplied user certificate
+ * @param[in] request_type The request type value.
+ * @param[in] key_id The request key ID.
+ * @param[in] data The request data to be wrapped/unwrapped (base64 encoded).
+ * @param[in] request_sig The digital signature for the request content (base64 encoded).
+ * @param[in] requestor_cert The requestor's X509 certificate (base64 encoded DER).
  *
  * @return 0 on success, 1 on error
  *
@@ -28,21 +28,34 @@ int validate_signature(RequestType * request_type, charbuf * key_id, charbuf * d
 
 /**
  * <pre>
- * Generate the signature for a signed pelz request.
+ * Generate the signature for a signed Pelz request.
  * This is intended for demonstration/testing purposes only because it runs outside the enclave.
  * <pre>
  *
  * @param[in] sign_pkey The requestor's private key.
  * @param[in] request_type The request type value.
  * @param[in] key_id The request key ID.
- * @param[in] key_id The request data to be wrapped/unwrapped.
- * @param[in] requestor_cert The requestor's X509 certificate.
+ * @param[in] data The request data to be wrapped/unwrapped (base64 encoded).
+ * @param[in] requestor_cert The requestor's X509 certificate (base64 encoded DER).
  *
  * @return A charbuf containing the signature, or an empty charbuf on failure.
  *
  */
 charbuf create_signature(EVP_PKEY * sign_pkey, RequestType * request_type, charbuf * key_id, charbuf * data, charbuf * requestor_cert);
 
+/**
+ * <pre>
+ * Serialize the contents of a signed Pelz request.
+ * <pre>
+ *
+ * @param[in] request_type The request type value.
+ * @param[in] key_id The request key ID.
+ * @param[in] data The request data to be wrapped/unwrapped (base64 encoded).
+ * @param[in] requestor_cert The requestor's X509 certificate (base64 encoded DER).
+ *
+ * @return A charbuf containing the serialization, or an empty charbuf on failure.
+ *
+ */
 charbuf serialize_request_data(RequestType * request_type, charbuf * key_id, charbuf * data, charbuf * requestor_cert);
 
 #endif /* INCLUDE_REQUEST_SIGNING_H_ */
