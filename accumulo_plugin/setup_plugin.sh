6336d60df94e07303015b8e59b3b1bcc0475d691
JWZeta <12718816+JWZeta@users.noreply.github.com>
2023-02-17 13:40:20 -0500

    Reorganize accumulo_plugin files and update setup_plugin.sh

--- accumulo_plugin/setup_plugin.sh
+++ accumulo_plugin/setup_plugin.sh
@@ -3,7 +3,6 @@
 install=false;
 uninstall=false;
 pykmip_demo=false;
-VERSION=plugin-accumulo;
 HELP=false;
 
 while getopts d:iuohp flag
@@ -12,7 +11,6 @@ do
 		d) DIR_ACCUMULO=${OPTARG};;
 		i) install=true;;
 		u) uninstall=true;;
-		o) VERSION+=-2.0;;
 		h) HELP=true;;
 		p) pykmip_demo=true;
 	esac
@@ -21,13 +19,12 @@ done
 if $HELP
 then
 	echo "setup_plugin.sh is used to install source files into Apache Accumulo"
-	echo -e "        Accumulo must be rebuilt after install in order to build the pelz plugin class files\n"
+	echo -e "\nAccumulo must be rebuilt after install in order to build the pelz plugin class files\n"
 	echo -e "usage: ./setup_plugin.sh [options]\n"
 	echo -e "options:\n"
 	echo " -d    Directory containing Accumulo code, must be specified"
 	echo " -i    Indicates 'install' of pelz plugin source code -- only installs java sources"
 	echo " -u    Indicates 'uninstall' of pelz plugin source code -- only removes java sources"
-	echo " -o    Install according to package structures as of the Accumulo 2.0 release"
 	echo " -h    Display help"
 	exit 0
 fi
@@ -41,13 +38,12 @@ then
 	then
 		#Install java plugin
 		mkdir -p $DIR_PELZ_PLUGIN
-		cp $VERSION/PelzCryptoService.java $DIR_PELZ_PLUGIN
-		cp pelzPlugin/* $DIR_PELZ_PLUGIN
+		cp pelz_plugin/plugin_code/* $DIR_PELZ_PLUGIN
 		#Install plugin tests for Accumulo
 		mkdir -p $DIR_PELZ_TESTS
-		cp $VERSION/testfiles/PelzCryptoTest.java $DIR_PELZ_TESTS
-		cp testfiles/RFilePelzTest.java $DIR_ACCUMULO/core/src/test/java/org/apache/accumulo/core/file/rfile/
-		cp testfiles/WriteAheadLogPelzEncryptedIT.java $DIR_ACCUMULO/test/src/main/java/org/apache/accumulo/test/functional/
+		cp pelz_plugin/testfiles/PelzCryptoTest.java $DIR_PELZ_TESTS
+		cp pelz_plugin/testfiles/RFilePelzTest.java $DIR_ACCUMULO/core/src/test/java/org/apache/accumulo/core/file/rfile/
+		cp pelz_plugin/testfiles/WriteAheadLogPelzEncryptedIT.java $DIR_ACCUMULO/test/src/main/java/org/apache/accumulo/test/functional/
 		echo "Install Complete"
     if $pykmip_demo
     then
